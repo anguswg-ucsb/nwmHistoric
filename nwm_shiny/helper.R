9d865a1028da6e59a33a62116aa5605de877df01
anguswg-ucsb <anguswatters@gmail.com>
2021-03-02 07:31:00 -0800

    NWM shiny 3/2/2021 v1

--- nwm_shiny/helper.R
+++ nwm_shiny/helper.R
@@ -18,44 +18,51 @@
 
 
 # USGS Water use data
-water_use <- dataRetrieval::readNWISuse(stateCd = "CA", countyCd = "Contra Costa") %>% 
-  janitor::clean_names()
+water_use_data <- function(state, county) {
+  water_use <- dataRetrieval::readNWISuse(stateCd = state, countyCd = county) %>%
+    janitor::clean_names()
+  water_use <- water_use %>%
+    select(1:6, contains("total_self_supplied_withdrawals_surface_water"), contains("surface_water_withdrawals_for_golf"))
+  water_use <- water_use %>%
+    mutate(across(5:22, as.numeric)) %>% 
+    replace(is.na(.), 0) 
+  water_use <- water_use %>%
+    rename(statefips = state_cd,
+           countyfips = county_cd,
+           pop = total_population_total_population_of_area_in_thousands,
+           livestock1 = livestock_stock_total_self_supplied_withdrawals_surface_water_in_mgal_d,
+           livestock2 = livestock_animal_specialties_total_self_supplied_withdrawals_surface_water_in_mgal_d,
+           therm1 = thermoelectric_power_closed_loop_cooling_total_self_supplied_withdrawals_surface_water_in_mgal_d,
+           therm2 = thermoelectric_power_once_through_cooling_total_self_supplied_withdrawals_surface_water_in_mgal,
+           aquacultere1 = aquaculture_total_self_supplied_withdrawals_surface_water_in_mgal_d,
+           irrigation1 = irrigation_total_total_self_supplied_withdrawals_surface_water_in_mgal_d,
+           irrigation2 = irrigation_golf_courses_self_supplied_surface_water_withdrawals_for_golf_courses_fresh_in_mgal_d)
+  
+  for ( col in 1:ncol(water_use)){
+    colnames(water_use)[col] <-  sub("_.*", "", colnames(water_use)[col])
+  }
 
-water_use <- water_use %>% 
-  select(1:6, contains("total_self_supplied_withdrawals_surface_water"), contains("surface_water_withdrawals_for_golf"))
-
-water_use <- water_use %>% 
-  mutate(across(5:22, as.numeric)) %>% 
-  replace(is.na(.), 0)
-
-water_use <- water_use %>% 
-  rename(statefips = state_cd,
-         countyfips = county_cd,
-         pop = total_population_total_population_of_area_in_thousands,
-         livestock1 = livestock_stock_total_self_supplied_withdrawals_surface_water_in_mgal_d,
-         livestock2 = livestock_animal_specialties_total_self_supplied_withdrawals_surface_water_in_mgal_d,
-         therm1 = thermoelectric_power_closed_loop_cooling_total_self_supplied_withdrawals_surface_water_in_mgal_d,
-         therm2 = thermoelectric_power_once_through_cooling_total_self_supplied_withdrawals_surface_water_in_mgal,
-         aquacultere1 = aquaculture_total_self_supplied_withdrawals_surface_water_in_mgal_d, 
-         irrigation1 = irrigation_total_total_self_supplied_withdrawals_surface_water_in_mgal_d,
-         irrigation2 = irrigation_golf_courses_self_supplied_surface_water_withdrawals_for_golf_courses_fresh_in_mgal_d) 
-
-for ( col in 1:ncol(water_use)){
-  colnames(water_use)[col] <-  sub("_.*", "", colnames(water_use)[col])
+  water_use <- water_use %>%
+    mutate("Public supply" = public + domestic,
+           Thermoelectric = total,
+           Irrigation = livestock1 + livestock2 + aquacultere1 + irrigation1 + irrigation2) %>%
+    select(1:6, "Public supply", Irrigation, Industrial = industrial, Mining = mining,  Thermoelectric)
+  water_use <- water_use %>%
+    tidyr::unite('fips', statefips, countyfips)
+  water_use$fips <- gsub("_", "", water_use$fips)
+  water_use$fips <- as.numeric(water_use$fips)
+  water_use <- water_use %>%
+    tidyr::pivot_longer(6:10, names_to = "sector", values_to = "withdrawals")
+  water_use <- group_by(water_use, sector, year)
 }
 
-water_use <- water_use %>% 
-  mutate(public_supply = public + domestic,
-         thermoelectric = total,
-         irrigation = livestock1 + livestock2 + aquacultere1 + irrigation1 + irrigation2) %>% 
-  select(1:6, public_supply, irrigation, industrial, mining,  thermoelectric)
-
-water_use <- water_use %>% 
-  tidyr::unite('fips', statefips, countyfips) 
-
-water_use$fips <- gsub("_", "", water_use$fips)
-water_use$fips <- as.numeric(water_use$fips)
-
+# catch_df <- get_nhdplus(comid = 101, realization = "catchment")
+# conus <- USAboundaries::us_counties() %>% 
+#   select(name, state_name)
+# tmp1 <- st_intersection(catch_df, conus) %>% 
+#   st_drop_geometry() %>% 
+#   slice(n =1)
+# plk = water_use_data(tmp1$state_name, tmp1$name)
 
 
 
@@ -290,6 +297,9 @@ make_table <- function(comid) {
                options = list(paging = TRUE, searching = TRUE))
 
 }
+# agg <- nwmHistoric::aggregate_ym(nwm)
+# hchart(agg, type = "column", hcaes(x = ym, y = flow_cms)) %>%
+#   hc_colors(c("red", "green", "blue", "grey", "orange"))
 
 
 make_table2 <- function(comid) {
